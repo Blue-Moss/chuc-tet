f341c00e42178c6e56ab560324bf3eecab02cb40
Vu Duc Hien <vuduchien865@gmail.com>
2026-02-04 09:24:38 +0700

    ok

--- js/script.js
+++ js/script.js
@@ -71,11 +71,11 @@ randomWords.forEach((word) => {
 
 // Ảnh dùng cho hiệu ứng nổ (hiển thị hình ảnh ngẫu nhiên tại điểm nổ)
 const imageSources = [
-	// "./images/image1.jpeg",
-	// "./images/image2.jpeg",
-	// "./images/image3.jpeg",
-	// "./images/image4.jpeg",
-	// "./images/image5.jpeg",
+	"./images/image1.jpeg",
+	"./images/image2.jpeg",
+	"./images/image3.jpeg",
+	"./images/image4.jpeg",
+	"./images/image5.jpeg",
 	"./images/1.JPG",
 	"./images/2.jpg",
 	"./images/3.jpg",
